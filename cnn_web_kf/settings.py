9b263060353b7ece985311818b90d74f47a3754c
Juan Manuel Curcio <juanmanuelcurcio@gmail.com>
2026-09-21 01:59:47 -0300

    csrf url

--- cnn_web_kf/settings.py
+++ cnn_web_kf/settings.py
@@ -23,6 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY_CNNKF")
 
+CSRF_TRUSTED_ORIGINS = [
+    "cnnauthorclassserving-production.up.railway.app",
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
